3096488f33d78f153b5dc58a3ecf14b79744c1c1
saurabhshcs <saurabhshcs@gmail.com>
2024-02-15 23:45:16 +0000

    Initial project setup

---
+++ src/main/java/com/saurabhshcs/springboot/aws/sqs/SpringbootAwsSqsApplication.java
@@ -0,0 +1,13 @@
+package com.saurabhshcs.springboot.aws.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsSqsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabhshcs/springboot/aws/sqs/SpringbootAwsSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabhshcs.springboot.aws.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
